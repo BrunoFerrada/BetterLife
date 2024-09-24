87e3d91118a797de019697deaf0fa6c80538ec41
MauroFerrada <mauroferrada01@gmail.com>
2024-09-24 03:40:42 -0300

    vistaRegistro

--- betterlifedj/betterlifereact/src/pages/Register/Register.jsx
+++ betterlifedj/betterlifereact/src/pages/Register/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button } from '../../components/Button/Button'; // Importamos el componente Button
 
 export const Register = () => {
   const [formData, setFormData] = useState({
@@ -44,38 +45,49 @@ export const Register = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username</label>
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          required
-        />
+    <div className="font-Nunito min-h-screen flex items-center justify-center">
+      <div className="bg-lime-700 p-8 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-2xl uppercase text-center mb-6 text-white">Registro</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-100 font-medium mb-2">Nombre de usuario</label>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+              className="bg-lime-700 w-full px-4 py-2 border-b-2 border-gray-300 focus:outline-none focus:border-lime-900 text-white transition-all duration-300 ease-in-out focus:border-b-lime-900"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-100 font-medium mb-2">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="bg-lime-700 w-full px-4 py-2 border-b-2 border-gray-300 focus:outline-none focus:border-lime-900 text-white transition-all duration-300 ease-in-out focus:border-b-lime-900"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-100 font-medium mb-2">Contraseña</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              className="bg-lime-700 w-full px-4 py-2 border-b-2 border-gray-300 focus:outline-none focus:border-lime-900 text-white transition-all duration-300 ease-in-out focus:border-b-lime-900"
+            />
+          </div>
+          {/* Centramos el botón */}
+          <div className="flex justify-center">
+            <Button text="Registrarse" onClick={handleSubmit} />
+          </div>
+        </form>
       </div>
-      <div>
-        <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit">Register</button>
-    </form>
+    </div>
   );
 };
