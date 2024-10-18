d2d19851a77d1aa1370fe875f2d00bc3a998426c
BrunoFerrada <brunoferrada1212@gmail.com>
2024-10-17 21:12:49 -0300

    "formulas" table created

--- betterlifedj/nutrition/models.py
+++ betterlifedj/nutrition/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import User
 
-
+class formulas():
+    idformula = models.AutoField(primary_key=True)
+    iduser = models.ForeignKey(User, on_delete=models.CASCADE)
+    formulaname = models.CharField("Nombre de la formula", max_length=50)
+    variables =  models.JSONField("Variables", null=True, blank=True)
+    resultado = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True) 
