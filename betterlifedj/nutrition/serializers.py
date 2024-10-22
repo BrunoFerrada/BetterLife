aacc0caac4f6238ce80d6157b8d401421c1c5a25
Fabricio <118219604+diazfabrici0@users.noreply.github.com>
2024-10-22 15:17:45 -0300

    cambios

--- betterlifedj/nutrition/serializers.py
+++ betterlifedj/nutrition/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from .models import Diet
 
 class CalculationSerializer(serializers.Serializer):
     
-    formula = serializers.ChoiceField(choices=[('harris', 'Harris-Benedict'), ('mifflin', 'Mifflin-St Jeor')])from rest_framework import serializers
+    formula = serializers.ChoiceField(choices=[('harris', 'Harris-Benedict'), ('mifflin', 'Mifflin-St Jeor')])
 from .models import Diet
 
 class DietSerializer(serializers.ModelSerializer):
